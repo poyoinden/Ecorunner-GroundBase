8e3b9f5bd4be560971d67f3a816b5d4314e7338f
poyoinden <32718357+poyoinden@users.noreply.github.com>
2018-06-19 17:31:23 +0200

    Add files via upload

--- gb/refactorGPS.py
+++ gb/refactorGPS.py
@@ -18,4 +18,34 @@ def toDecimal(lon, lat):
 	return latres, lonres
 
 if __name__ == "__main__":
-	print toDecimal('00422.530132', '5159.981901')
+#	print toDecimal('00422.530132', '5159.981901')
+#	print toDecimal('00202.234470', '4906.195493')
+#	print toDecimal('00202.249846', '4906.178389')
+#	print toDecimal('00202.250278', '4906.181853')
+#	print toDecimal('00202.250330', '4906.181930')
+#	print toDecimal('00422.573848', '5159.972468')
+#	print toDecimal('00422.602826', '5159.981004')
+#	print toDecimal('00422.569904', '5159.976496')
+#	print toDecimal('00422.577231', '5159.979423')
+#	print toDecimal('00422.587480', '5159.982014')
+#	print toDecimal('00422.597289', '5159.983450')
+#	print toDecimal('00422.602115', '5159.981409')
+#	print toDecimal('00422.605359', '5159.979105')
+#	print toDecimal('00422.604023', '5159.975464')
+#	print toDecimal('00422.602313', '5159.973968')
+#	print toDecimal('00422.599168', '5159.973880')
+#	print toDecimal('00422.579619', '5159.971361')
+#	print toDecimal('00422.573848', '5159.972467')
+#	print toDecimal('00422.582082', '5159.969553')
+
+	print toDecimal('00530.841581', '5227.418689')
+
+
+
+
+#templates:
+#type-safe, expressive, and can increase runtime performance by both shifting computation to compile-time and enabling optimizations that can't be applied to equivalent runtime code.
+#
+#
+#
+
